98f6de27e58eb9e3fa3f7e3b263360db4def9eec
Jon Preizler <42856286+jonpreizler@users.noreply.github.com>
2018-09-03 20:44:02 -0700

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=preizler-qna-test-Web-Deploy.pubxml -p:Password=3XMSYvzzphHL4AjouJAQmcyDKxma37uJDwiBpiy0yiQBGiraftsZgqChRNs1
+
